91907e9e599a6ea8dddf2ea7b5b40a4ddbd4b6f9
Takumi Oyamada <165040846+takumi0706@users.noreply.github.com>
2025-03-19 17:15:50 +0900

    Update test setup file to not be treated as a test

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -6,7 +6,5 @@ process.env.GOOGLE_CLIENT_ID = 'test-client-id';
 process.env.GOOGLE_CLIENT_SECRET = 'test-client-secret';
 process.env.GOOGLE_REDIRECT_URI = 'http://localhost:3000/oauth2callback';
 
-// テスト終了時にクリーンアップ処理を行う
-afterAll(() => {
-  // 必要に応じてクリーンアップ処理を追加
-});
+// このファイルはテストではないことを明示
+export default {};
